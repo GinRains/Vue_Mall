47c3f529d2265107efd3f770520730fa899fe797
Gin <3170066383@qq.com>
2020-07-27 00:35:47 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,12 @@
 <template>
   <div>
-    <Header></Header>
-    <router-view></router-view>
-    <Footer v-if="!$route.meta.isHide"></Footer>
+
   </div>
 </template>
 
 <script>
-  import Header from "@/components/Header"
-  import Footer from "@/components/Footer"
-
   export default {
-    name: "App",
-    mounted() {
-      this.getCategoryList()
-    },
-    methods: {
-      getCategoryList() {
-        this.$store.dispatch("getCategoryList")
-      }
-    },
-    components: {
-      Header,
-      Footer
-    }
+    name: "App"
   }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,44 +1,6 @@
-import Vue from 'vue'
-import VueRouter from "vue-router"
-import App from './App.vue'
-import router from "./router"
-import store from "./store"
-import "./mock/mockServe"
-
-import TypeNav from "./components/TypeNav"
-import SlideShow from "./components/SlideShow"
-import Pagination from "./components/Pagination"
-
-Vue.config.productionTip = false
-
-const originPush = VueRouter.prototype.push
-const originReplace = VueRouter.prototype.replace
-
-VueRouter.prototype.push = function (location, onResolved, onRejected) {
-  if(onResolved === undefined && onRejected === undefined) {
-    originPush.call(this, location).catch(() => {})
-  }else {
-    originPush.call(this, location, onResolved, onRejected)
-  }
-}
-
-VueRouter.prototype.replace = function (location, onResolved, onRejected) {
-  if(onResolved === undefined && onRejected === undefined) {
-    originReplace.call(this, location).catch(() => {})
-  }else {
-    originReplace.call(this, location, onResolved, onRejected)
-  }
-}
-
-Vue.component("TypeNav", TypeNav)
-Vue.component("SlideShow", SlideShow)
-Vue.component("Pagination", Pagination)
+import Vue from "vue"
+import App from "./App"
 
 new Vue({
-  beforeCreate() {
-    Vue.prototype.$bus = this
-  },
-  render: h => h(App),
-  router,
-  store
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app")
